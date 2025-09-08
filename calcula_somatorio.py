a7763fe3d7ab947f2ed18ff626f8ef635daaa6f2
LeizaneMelo <60352187+LeizaneMelo@users.noreply.github.com>
2025-09-07 22:20:40 -0300

    Update calcula_somatorio.py
    "Resolução do exercício 05 - calcula_somatorio"

--- calcula_somatorio.py
+++ calcula_somatorio.py
@@ -1,1 +1,10 @@
-# Sua solução aqui
+# lê n do usuário
+n = int(input("Digite um número inteiro n: "))
+soma = 0 # acumula o somatório
+
+# só calcula se n for pelo menos 2
+if n >= 2:
+    for i in range(2, n + 1): # percorre valores de 2 até n
+        soma += 1 / (i - 1) # adiciona o termo 1/(i-1)
+# exibe resultado
+print(f"{soma:.2f}")
